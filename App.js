7d19ad7ad4f42787331514fa4bf11dbd029bfb65
nipunChathura <nipunc1999@gmail.com>
2022-02-18 05:56:43 +0530

    Responsive

--- App.js
+++ App.js
@@ -2,25 +2,22 @@ import React, {useState} from 'react';
 import {Button, StyleSheet, Text, View} from 'react-native';
 
 const App = () => {
-  const [name, setName] = useState('Nipun');
-  const [session, setSession] = useState({number: 6, title: 'state'});
-  const [current, setCurrent] = useState(true);
+  const [name, setName] = useState('Style Test');
 
   const onClickHandler = () => {
-    setName('Nipun Chathuranga');
-    setSession({number: 7, title: 'Style'});
-    setCurrent(false);
+    setName('Style Test is Done');
   };
   return (
     <View style={styles.body}>
-      <Text style={styles.text}>My Name is {name}</Text>
-      <Text style={styles.text}>
-        This is session number {session.number} and adout {session.title}
-      </Text>
-      <Text style={styles.text}>
-        {current ? 'current session' : 'next session'}
-      </Text>
-      <Button title="Click" onPress={onClickHandler} />
+      <View style={styles.view1}>
+        <Text style={styles.text}>1</Text>
+      </View>
+      <View style={styles.view2}>
+        <Text style={styles.text}>2</Text>
+      </View>
+      <View style={styles.view3}>
+        <Text style={styles.text}>3</Text>
+      </View>
     </View>
   );
 };
@@ -28,12 +25,34 @@ const App = () => {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#0000ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  view1: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#00ffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view2: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#ff00ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view3: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#00ff00',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   text: {
-    color: '#ffffff',
+    color: '#000000',
     fontSize: 20,
     fontStyle: 'italic',
     margin: 20,
